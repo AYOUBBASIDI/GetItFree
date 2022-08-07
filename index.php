30d2f232b95f149a719e244abc9c46b2266a8a77
Ayoub Basidi <93930160+AYOUBBASIDI@users.noreply.github.com>
2022-08-07 15:58:52 +0100

    changes made on Sun Aug  7 15:58:52 MDT 2022

--- index.php
+++ index.php
@@ -83,4 +83,15 @@
 
 
 <script src="App.js">
+
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/62efd2f254f06e12d88d608f/1g9sd01kq';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
+
 </script>
